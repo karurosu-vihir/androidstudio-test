00765af2862aded6a6af55b59ff6fcecee6278a5
LizVenus07 <lizethferrufino07@gmail.com>
2024-06-26 23:11:56 -0600

    Actividad Principal Nueva

--- app/src/main/java/com/example/messi/MainActivity.kt
+++ app/src/main/java/com/example/messi/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.messi
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val informacionpersonal: Button = findViewById(R.id.btnInfoPersonal)
+        val premios: Button = findViewById(R.id.btnpremios)
+        val goles: Button = findViewById(R.id.btngoles)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
