adc8cda31f5001fb51f5b7bda9feccef008084bf
dry <karurosu.vihir@gmail.com>
2024-06-26 22:55:09 -0600

    commit 26/6

--- app/src/main/java/com/example/messi/InfoGeneral.kt
+++ app/src/main/java/com/example/messi/InfoGeneral.kt
@@ -1,14 +1,17 @@
 package com.example.messi
 
+import android.database.Cursor
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ListView
+import android.widget.SimpleCursorAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class InfoGeneral : AppCompatActivity() {
+    private lateinit var baseHelper: BaseDatosHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,12 +21,22 @@ class InfoGeneral : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        baseHelper = BaseDatosHelper(this)
+
+        baseHelper.insertarDatos("Nombre", "Lionel Messi")
+        baseHelper.insertarDatos("Edad", "34 años")
+
 //        Variables
         val atras = findViewById<Button>(R.id.button)
         val lista = findViewById<ListView>(R.id.lista)
 
 //        Elemento del adapter
+        val datos: Cursor = baseHelper.obtenerDatos()
+        val columnas = arrayOf( BaseDatosHelper.COLUMNA_ID,BaseDatosHelper.COLUMNA_TITULO, BaseDatosHelper.CONTENIDO_TABLA)
         val elemvista =intArrayOf(R.id.idItem, R.id.titulo, R.id.contenido)
+
+        val adaptador = SimpleCursorAdapter(this, R.layout.iteminfogeneral, datos, columnas, elemvista, 0)
+        lista.adapter = adaptador
 
 //        Salir
         atras.setOnClickListener {
--- app/src/main/java/com/example/messi/MainActivity.kt
+++ app/src/main/java/com/example/messi/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.messi
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val boton = findViewById<Button>(R.id.button2)
+        boton.setOnClickListener {
+            val intent = Intent(this, InfoGeneral::class.java)
+            startActivity(intent)
+        }
         baseHelper = BaseDatosHelper(this)
     }
 }
