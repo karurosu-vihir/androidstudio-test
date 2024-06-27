183d7ed287cca52eabc3209509c196ea42c10758
MajoPosas <maposas376@unicah.edu>
2024-06-26 22:05:44 -0600

    InfoGeneral 2

--- app/src/main/java/com/example/messi/BaseDatosHelper.kt
+++ app/src/main/java/com/example/messi/BaseDatosHelper.kt
@@ -9,19 +9,19 @@ class BaseDatosHelper (context: Context) : SQLiteOpenHelper(context, NOMBRE_BASE
     companion object{
         val NOMBRE_BASE_DATOS = "InfoGeneral.db"
         val VERSION_BASE_DATOS = 1
-        private const val NOMBRE_TABLA = "datosmessi"
+        private const val TABLA_PREMIOS = "datosmessi"
         private const val COLUMNA_TITULO = "titulo"
         private const val COLUMNA_ID = "id"
         private const val CONTENIDO_TABLA = "contenido"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val sql = "CREATE TABLE $NOMBRE_TABLA ($COLUMNA_ID INTEGER PRIMARY KEY AUTOINCREMENT, $COLUMNA_TITULO TEXT, $CONTENIDO_TABLA TEXT)"
+        val sql = "CREATE TABLE $TABLA_PREMIOS ($COLUMNA_ID INTEGER PRIMARY KEY AUTOINCREMENT, $COLUMNA_TITULO TEXT, $CONTENIDO_TABLA TEXT)"
         db?.execSQL(sql)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        val sql = "DROP TABLE IF EXISTS $NOMBRE_TABLA"
+        val sql = "DROP TABLE IF EXISTS $TABLA_PREMIOS"
         db?.execSQL(sql)
         onCreate(db)
     }
@@ -31,14 +31,13 @@ class BaseDatosHelper (context: Context) : SQLiteOpenHelper(context, NOMBRE_BASE
             put(COLUMNA_ID, 1)
             put(COLUMNA_TITULO, "Lionel Andrés Messi Cuccittini")
             put(CONTENIDO_TABLA,"Lionel Andrés Messi Cuccittini nació el 24 de junio de 1987 (36 años) en Rosario, " +
-                    "Argentina. Es reconocido por muchos como el mejor " +
-                    "futbolista y uno de los mejores deportistas de todos los tiempos, ha ganado 44 títulos colectivos y más " +
+                    "Argentina. Es reconocido por muchos como el mejor futbolista y uno de los mejores deportistas de todos los tiempos, ha ganado 44 títulos colectivos y más " +
                     "de 50 títulos individuales en su carrera. Es el jugador con más Balones de Oro y uno de los pocos en ganar " +
                     "una medalla de oro en los juegos olímpicos y un Mundial.")
         }
 
         if (db != null) {
-            db.insert(NOMBRE_TABLA, null, values)
+            db.insert(TABLA_PREMIOS, null, values)
         }
 
     }
